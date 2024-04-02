7969f2fa2fd660339403c195c7685cf34b25a156
Andrew1407 <endry1407@gmail.com>
2024-04-02 11:07:20 +0300

    LinkAddressAction modified: custom target button property added

--- Assets/Scripts/UI/LinkAddressAction.cs
+++ Assets/Scripts/UI/LinkAddressAction.cs
@@ -6,20 +6,18 @@ public class LinkAddressAction : MonoBehaviour
 {
     [SerializeField] private string _urlAddress;
 
+    [SerializeField] private Button _actionButton;
+
     public static bool IsValidURL(string url)
     {
-        Uri uriResult;
-        bool isValidUrl = Uri.TryCreate(url, UriKind.Absolute, out uriResult)
-            && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
-
-        return isValidUrl;
+        bool isUrlCreated = Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult);
+        return isUrlCreated && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        var button = GetComponent<Button>();
-        if (button != null) button.onClick.AddListener(handleOnClick);
+        if (_actionButton == null) _actionButton = GetComponent<Button>();
+        _actionButton?.onClick.AddListener(handleOnClick);
     }
 
     private void handleOnClick()
